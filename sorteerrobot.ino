1bb5e554d5a2b9ae3b73f0f1b4968f2cf90cd42b
harmenn <harmen_meijden@hotmail.com>
2017-05-24 13:20:52 +0200

    .

--- sorteerrobot.ino
+++ sorteerrobot.ino
@@ -4,7 +4,6 @@ Motor motor1(7, 6, 240);
 
 Motor motor2(4, 5, 240);
 int loc = 0;
-
 int products = 0;
 void setup() {
   Serial.begin(9600);
@@ -24,7 +23,7 @@ void loop() {
         motor1.driveLeft(750);
         delay(200);
       }
-      motor2.driveLeft(375);
+      motor2.driveLeft(450);
       Serial.println("bpp-status-sort_succes");
     } else if (ser == "command-rotate_right") {
       if(loc==0 || loc==1){
@@ -32,7 +31,7 @@ void loop() {
         motor1.driveRight(750);
         delay(200);
       }
-      motor2.driveLeft(375);
+      motor2.driveLeft(450);
       Serial.println("bpp-status-sort_succes");
     } else if (ser == "command-arm_reset") {
       motor2.driveLeft(2000);
@@ -42,25 +41,27 @@ void loop() {
       if (products == 0) {
         motor2.driveRight(2500);
       } else if (products == 1) {
-        motor2.driveRight(1000);
+        motor2.driveRight(900);
       } else if (products == 2) {
+        motor2.driveRight(900);
+      } else if (products == 3) {
         motor2.driveRight(900);
       } else {
         motor2.driveRight(900);
-        products = 0;
+        //products = 0;
       }
       products++;
       Serial.println("bpp-status-arm_out_ok");
 
     } else if (ser == "command-arm_in") {
       if (products == 1) {
-        motor2.driveLeft(1100);
+        motor2.driveLeft(1200);
       } else if (products == 2) {
-        motor2.driveLeft(1100);
+        motor2.driveLeft(1200);
       } else if (products == 3) {
-        motor2.driveLeft(800);
+        motor2.driveLeft(1200);
       } else {
-        motor2.driveLeft(1300);
+        motor2.driveLeft(1400);
       }
       Serial.println("bpp-status-arm_in_ok");
     } else if (ser == "command-arm_all_in") {
